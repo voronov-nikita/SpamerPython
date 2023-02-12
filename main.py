db54df9ae092859f3e61b4f81c53a8d068910a93
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-02-12 13:55:34 +0300

    main_spam

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pyautogui
 import time
 
+
 def send_message():
-    message = input()
-    amount = 30
+    message = input("Text:")
+    amount = 100
     time.sleep(6)
-    
+
     for i in range(amount):
         pass
-    
-    while amount>0:
-        amount-=1
-        
+
+    while amount > 0:
+        amount -= 1
+
         pyautogui.typewrite(message.strip())
         pyautogui.press("enter")
-        
+
+
 send_message()
