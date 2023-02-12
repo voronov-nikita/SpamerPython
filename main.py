4e7ee20db9a937618c3286f23e385e3d28b262c8
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-02-12 14:12:05 +0300

    main

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import pyautogui
 import time
-
+import keyboard
 
 def send_message():
     message = input("Text:")
-    amount = 100
+    amount = 400
     time.sleep(6)
 
     for i in range(amount):
         pass
 
     while amount > 0:
-        amount -= 1
+        if keyboard.is_pressed("esc"):
+            break
+        else:
+            amount -= 1
 
-        pyautogui.typewrite(message.strip())
-        pyautogui.press("enter")
+            pyautogui.typewrite(message.strip())
+            pyautogui.press("enter")
 
 
 send_message()
